517debb497b784da434eaf03608a6902a34523a9
Avani Somaiya <avani.somaiya@browseinfo.in>
2022-08-23 11:27:41 +0530

    MODULE ADDED

---
+++ bi_pos_scan_mobile_barcode/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name" : "POS Mobile Barcode Scanner | POS Mobile QRCode Scanner",
+    "version" : "14.0.0.1",
+    "category" : "",
+    'summary': 'POS Mobile Barcode Scanner | POS Mobile QRCode Scanner',
+    "description": """
+    
+   Description of the module. 
+    
+    """,
+    "author": "BrowseInfo",
+    "website" : "www.browseinfo.in",
+    "price": 000,
+    "currency": 'EUR',
+    "depends" : ['base', 'point_of_sale','bi_qr_generator'],
+    "data": [
+        'views/pos_assets.xml',
+        'views/pos_config_view.xml',
+    ],
+    'qweb': ['static/src/xml/pos.xml'
+    ],
+    "auto_install": False,
+    "installable": True,
+    "live_test_url":'youtube link',
+    "images":["static/description/Banner.png"],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ bi_qr_generator/Readme.txt
@@ -0,0 +1,2 @@
+Version 14.0.0.1 : (18/08/22)
+    - Change qr_code filed for auto update when change into product template change into product product add _sql_constraints for product QR Code.
